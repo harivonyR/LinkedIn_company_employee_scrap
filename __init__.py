47f4e1d2ffa6edb104dd82e2657507a8df02abfa
HarivonyR <harivonyratefiarison@gmail.com>
2025-06-24 20:50:57 +0300

    initial commit

---
+++ __init__.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jun 24 20:46:29 2025
+
+@author: BEST
+"""
+
+from credential import x_api_key
+
---
+++ main.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jun 24 20:42:08 2025
+
+@author: Harivony RATEFIARISON
+"""
+
