0afb088317889070f3f1497856432a3caec539a4
HarivonyR <harivonyratefiarison@gmail.com>
2025-06-26 12:53:40 +0300

    OK : dork + google search sample + get profil info

--- main.py
+++ main.py
@@ -5,27 +5,38 @@ Created on Tue Jun 24 20:42:08 2025
 @author: Harivony RATEFIARISON
 """
 
+# LOAD API KEY
 
 from credential import x_api_key
+
+#
+#
+# 0 - BUILD DORK (syntax)
+#
+# 
+
+# Dork : "intext:\'Apple Inc.\' inurl:in site:linkedin.com -inurl:posts"
+
+# intext:\'Apple Inc.\' 
+#site:linkedin.com 
+# inurl:in 
+#-inurl:posts"
+
+
+#
+#
+# I - Get company employee linkedIn profil link List 
+#
+# 
+
 import requests
 
 url = "https://piloterr.com/api/v2/google/search"
 
-query = "intext:\"Apple\" site:linkedin.com -inurl:posts/ -inurl:company/ -inurl:video/ -inurl:pulse/ -inurl:pulse/ inurl:in/"
-query_test = "linkedIn data analyst"
-location = "NY"
-gl = "us"
-page = 2
-num = 10 # maximum number of result to return
 
 payload = {
-    "query": query_test,
-    #"location": location,
-    #"uule": "<string>", # can not be used with location param
-    #"gl": gl,
-    #"hl": "<string>",
-    #"page": page,
-    #"num": num
+    "query": "intext:\'Apple Inc.\' inurl:in site:linkedin.com -inurl:posts",
+    "page" : 1
 }
 
 headers = {
@@ -35,4 +46,65 @@ headers = {
 
 response = requests.request("POST", url, json=payload, headers=headers)
 
-print(response.text)
+print("------------")
+print(f"1) google search response : {response.text}")
+
+
+#
+#
+# II - Get profil info 
+#
+# 
+
+# 1 - get link
+
+# Local json load / server temporary anavalaible
+import json
+with open("input/google_search_api_response.json", "r", encoding="utf-8") as f:
+    data = json.load(f)
+    
+#data = response.json()
+result_organic = data['organic_results']       # result_organic is the search result key that contain profile link list
+profil_link = result_organic[0]['link']        # sample profile link
+
+print("------------")
+print(f"2) sample profile link : {profil_link}")
+
+
+# 2 - fetch profile info using API
+
+
+#
+#
+# III - Get profil info 
+#
+# 
+
+import requests
+
+url = "https://piloterr.com/api/v2/linkedin/advanced/profile/info"
+
+headers = {
+    "x-api-key": x_api_key
+}
+
+querystring = {"query" : profil_link} 
+
+response = requests.request("GET", url, headers=headers, params=querystring)
+
+print(f"3) sample profile link : {response.json()}")
+
+#
+#
+# IV - LOOP process - merge data
+#
+# 
+
+
+
+
+
+
+
+
+
